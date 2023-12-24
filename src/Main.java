888d03bdca9e190cc2ce1985b6c823dff6f7b11d
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2023-12-24 09:14:46 +0100

    add catch exception

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import core.Bootstrap;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            new Bootstrap();
+        } catch (Exception exception) {
+            JOptionPane.showMessageDialog(
+                    new JFrame(),
+                    exception.getClass().getSimpleName() + " in " + exception.getStackTrace()[0],
+                    "Es ist ein Fehler aufgetreten",
+                    JOptionPane.ERROR_MESSAGE
+            );
+        }
+
     }
+
 }
