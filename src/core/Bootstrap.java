74433c6fa1800fa46d1f6baccefc2baa8d5dbac7
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2023-12-24 09:15:08 +0100

    throw test exception

--- src/core/Bootstrap.java
+++ src/core/Bootstrap.java
@@ -1,2 +1,22 @@
-package core;public class Bootstrap {
+package core;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Bootstrap extends JFrame {
+
+    private final Integer[] numbers = new Integer[5];
+
+    public Bootstrap() {
+        init();
+        System.out.println(numbers[4]);
+    }
+
+    private void init() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(800,600);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
 }
