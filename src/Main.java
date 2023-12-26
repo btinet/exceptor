99a1c0cc7e40658c94ed416d7f7ccdb6c7f34d4d
Benjamin Wagner <51082373+btinet@users.noreply.github.com>
2023-12-26 15:12:21 +0100

    remove comments

--- src/Main.java
+++ src/Main.java
@@ -11,16 +11,12 @@ public class Main {
     public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
 
         Thread.setDefaultUncaughtExceptionHandler(new ExceptionHandler());
+
         // ExceptionHandler.DEV = true;
-        //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
         UIManager.setLookAndFeel(
                 UIManager.getSystemLookAndFeelClassName()
         );
-        /* Turn off metal's use of bold fonts */
-        UIManager.put("swing.boldMetal", Boolean.FALSE);
 
-        //Schedule a job for the event dispatch thread:
-        //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(Bootstrap::new);
 
     }
